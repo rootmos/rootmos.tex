2594e7dcbac868a736d7a3c796cd939646c74824
Gustav Behm <me@rootmos.io>
2026-06-08 12:59:13 +0200

    Make the included meta information configurable
    template/sandbox.tex: 5 (+6=11) words

--- texmf/tex/luatex/rootmos/rootmos.lua
+++ texmf/tex/luatex/rootmos/rootmos.lua
@@ -1,4 +1,18 @@
-local M = {}
+local M = {
+    meta = {
+        buildinfo = true,
+        final = {
+            wordcount = {
+                sum = { "text", "other" },
+            },
+        },
+        draft = {
+            wordcount = {
+                sub = { "text", "other" },
+            },
+        },
+    },
+}
 
 local def = require("rootmos-utils").def
 local luaaux = require("rootmos-utils").luaaux
@@ -34,29 +48,90 @@ end
 
 function M.buildinfo(path)
     local B = luaaux(path, "build-info.lua")
-    tex.print(string.format("\\href{%s}{\\tt %s}", B.git_commit_url, B.time))
-    tex.print(string.format("\\href{%s}{\\tt %s}", B.git_commit_url, B.git_ref))
+    local s = string.format("\\href{%s}{\\tt %s}", B.git_commit_url, B.time)
+    s = s .. string.format(" \\href{%s}{\\tt %s}", B.git_commit_url, B.git_ref)
     if B.git_dirty then
-        tex.print("{\\tt(dirty)}")
+        s = s .. " {\\tt(dirty)}"
     end
+    return s
 end
 
-function M.wordcount(path, other)
+function M.wordcount(path, cfg)
     local W = luaaux(path, "wc")
 
-    tex.print(tostring(W.text))
+    local s = ""
 
-    if other == nil then
-        other = M.jobtype == "draft"
+    local sum = cfg.sum
+    if sum then
+        local acc = 0
+        for _, k in ipairs(sum) do
+            local w = W[k] or 0
+            acc = acc + w
+        end
+        s = tostring(acc)
     end
-    if other then
-        tex.print(string.format("(+%d=%d)", W.other, W.text + W.other))
+
+    local sub = cfg.sub
+    if sub then
+        local acc = 0
+        local t = ""
+        for _, k in ipairs(sub) do
+            local w = W[k] or 0
+            acc = acc + w
+            if t ~= "" then
+                t = t .. "+"
+            end
+            t = t .. tostring(w)
+        end
+        if s == "" then
+            s = string.format("%s=%d", t, acc)
+        else
+            s = s .. string.format(" (%s=%d)", t, acc)
+        end
     end
+
+    return s
 end
 
 function M.setup()
     M.setup_jobtype()
     M.setup_addbibresources()
+end
+
+function M.print_meta()
+    local meta = M.meta
+    if not meta then
+        return
+    end
+    jt = meta[M.jobtype] or {}
+
+    local function cfg(k)
+        local v = jt[k]
+        if v ~= nil then
+            return v
+        end
+        return meta[k]
+    end
+
+    local wc = cfg("wordcount")
+    if wc then
+        tex.print {
+            [[\footnotetext{]],
+            [[\iftoggle{@swedish}{Antal ord}{Word count}:]],
+            M.wordcount(nil, wc),
+            [[}]],
+        }
+    end
+
+    local bi = cfg("buildinfo")
+    if bi then
+        tex.print {
+            [[\footnotetext{]],
+            M.buildinfo(),
+            [[}]],
+        }
+    end
+
 end
 
 return M
